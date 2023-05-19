18c838ec822b3d6426658a3bde2f9391b1d20be9
F4RAN <F4RAN@vitalize.dev>
2023-05-20 02:56:32 +0330

    Update install-termux.sh

--- install-termux.sh
+++ install-termux.sh
@@ -54,7 +54,7 @@ pip install virtualenv
 virtualenv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-python app.py
+python app.py &
 
 echo "Configure Boot Settings"
 BOOT_DIRECTORY="$HOME/.termux/boot"
@@ -72,7 +72,7 @@ cat > "$SCRIPT_FILE" << EOF
 #!/data/data/com.termux/files/usr/bin/bash
 termux-wake-lock && sshd
 project_path=$(readlink -f "./$DIRECTORY")
-cd "$project_path" && source .venv/bin/activate && python3 app.py &
+cd "$project_path" && source .venv/bin/activate && python3 app.py
 EOF
 
 # Make the script executable
