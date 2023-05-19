596f5a708a7d8c02c7921bed91a96d01b14660dd
F4RAN <F4RAN@vitalize.dev>
2023-05-20 03:23:02 +0330

    Update app.py > try except format

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+
 import requests
 from flask import Flask, request
 from flask_cors import CORS
@@ -10,20 +11,30 @@ CORS(app, resources={r"/*": {"origins": ["https://dev.vitalize.dev", "192.168.1.
 @app.route("/print", methods=["POST"])
 def print_receipt():
     content = request.args.get("hex")
-    res = requests.get(f"http://192.168.1.100/?hex={content}")
-    if res.json()['success']:
-        return {'success': True}
-    else:
-        return {'success': False}
+    if not content:
+        return {'success': False, 'message': "Content not found"}
+    try:
+        res = requests.get(f"http://192.168.1.100/?hex={content}")
+        if res.json()['success']:
+            return {'success': True}
+        else:
+            return {'success': False}
+    except Exception as e:
+        print(e)
+        return {'success': False, 'message': e}
 
 
 @app.route("/print/cut", methods=["POST"])
 def cut_receipt():
-    res = requests.get(f"http://192.168.1.100/?cutpaper")
-    if res.json()['success']:
-        return {'success': True}
-    else:
-        return {'success': False}
+    try:
+        res = requests.get(f"http://192.168.1.100/?cutpaper")
+        if res.json() and res.json()['success']:
+            return {'success': True}
+        else:
+            return {'success': False}
+    except Exception as e:
+        print(e)
+        return {'success': False, 'message': e}
 
 
 @app.route("/check_printer", methods=["GET"])
@@ -32,11 +43,15 @@ def check_printer():
     config = json.loads(printer_config.read())
     ip = config['ip']
     port = config['port']
-    res = requests.get(ip + ":" + port + '/?selftest')
-    if res.json() and res.json()['success']:
-        return {'success': True}
-    else:
-        return {'success': False}
+    try:
+        res = requests.get(ip + ":" + str(port) + '/?selftest')
+        if res.json() and res.json()['success']:
+            return {'success': True}
+        else:
+            return {'success': False}
+    except Exception as e:
+        print(e)
+        return {'success': False, 'message': e}
 
 
 @app.route("/find_printer", methods=["GET"])
