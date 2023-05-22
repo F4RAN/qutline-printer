ddb85b6b40ce93da55f812aabfbfef39a7fea67a
F4RAN <F4RAN@vitalize.dev>
2023-05-22 05:22:19 +0330

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,15 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["https://dev.vitalize.dev", "192.168.1.100:80"]}})
 
 
-@app.route("/print", methods=["POST"])
+@app.route("/print", methods=["GET", "OPTIONS"])
 def print_receipt():
+    if request.method == "OPTIONS":
+        # Return the appropriate CORS headers
+        response = make_response()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Methods", "GET, OPTIONS")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+        return response
     content = request.args.get("hex")
     if not content:
         return {'success': False, 'message': "Content not found"}
