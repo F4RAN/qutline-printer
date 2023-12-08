a6311ce9fdd69f342d9e5cb466a1add9b8d785bd
F4RAN <parsa.hassani30@gmail.com>
2023-12-09 01:01:56 +0330

    instead of image path image data

--- app.py
+++ app.py
@@ -82,13 +82,14 @@ def print_receipt():
         return jsonify({'success': False, 'message': 'No image file selected'})
 
     # Save the image file to a desired location
-    dt = datetime.now()
-    now = str(dt.year) + str(dt.month) + str(dt.day) + str(dt.second) + str(dt.microsecond)
-    image_path = os.path.join("./", now + image_file.filename)
-    image_file.save(image_path)
-    image_file.close()
+    # dt = datetime.now()
+    # now = str(dt.year) + str(dt.month) + str(dt.day) + str(dt.second) + str(dt.microsecond)
+    # image_path = os.path.join("./", now + image_file.filename)
+    # image_file.save(image_path)
+    # image_file.close()
+    image_data = image_file.read()
     try:
-        res = print_base64(image_path,db)
+        res = print_base64(image_data,db)
         if res:
             return {'success': True}
         else:
