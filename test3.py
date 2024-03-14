23eca7f8b1961737b4916cc6d51aef6113ef4300
Arian Amini <arian.amini92@gmail.com>
2024-03-14 14:32:18 -0400

    a

--- test3.py
+++ test3.py
@@ -1,11 +1,26 @@
-import sqlite3
-DATABASE = "dbs/database.sqlite"
-# stores = cursor.execute("SELECT * FROM Job").fetchall()
+import requests
+import re
+tout = 50
+def get_printer_details():
+    ip = "192.168.1.159"
+    headers = {
+        'Authorization': 'Basic YWRtaW46YWRtaW4=',
+        'Origin': f'http://{ip}',
+        'Referer': f'http://{ip}/wireless_en.html',
+    }
+    res = requests.get("http://" + ip + '/wireless_en.html', headers=headers, timeout=tout)
+    # print(res.content)
+    pattern = r'var\s+(\w+)\s*=\s*"([^"]+)"'
+    matches = re.findall(pattern, str(res.content))
+    result = dict(matches)
+    print(result)
+    payload = f'sta_setting_encry={result["sta_setting_encry"]}&sta_setting_auth={result["sta_setting_auth"]}&sta_setting_ssid={result["sta_setting_ssid"]}&sta_setting_auth_sel={result["sta_setting_auth_sel"]}&sta_setting_encry_sel={result["sta_setting_encry_sel"]}&sta_setting_type_sel={result["sta_setting_type_sel"]}&sta_setting_wpakey={result["sta_setting_wpakey"]}&wan_setting_dhcp={result["wan_setting_dhcp"]}&wan_setting_ip={result["wan_setting_ip"]}&wan_setting_msk={result["wan_setting_msk"]}&wan_setting_gw={result["wan_setting_gw"]}&wan_setting_dns={result["wan_setting_dns"]}'
+    try:
+        res = requests.post("http://" + ip + '/do_cmd_en.html', headers=headers, data=payload, timeout=tout)
+        res2 = requests.post("http://" + ip + "/success_en.html", headers=headers, data='HF_PROCESS_CMD=RESTART',
+                             timeout=tout)
+    except Exception as e:
+        print(e)
 
-def get_db():
-    db = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    return cursor
-cursor = get_db()
-cursor.execute("INSERT INTO Printer (name, mac_addr, ip_addr, access_level, connection) VALUES (?,?,?,?,?)",
-                   ("Test", "0C:7F:ED:C0:E0:4C", "192.168.1.159", 0, 1))
+
+get_printer_details()
