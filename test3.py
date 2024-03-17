347136a165f21778f2c8eb90e99a97eeb2b6baba
Arian Amini <arian.amini92@gmail.com>
2024-03-17 19:02:07 -0400

    make static / dynamic

--- test3.py
+++ test3.py
@@ -1,7 +1,7 @@
 import requests
 import re
 tout = 50
-def get_printer_details():
+def set_printer_ip_static():
     ip = "192.168.1.159"
     headers = {
         'Authorization': 'Basic YWRtaW46YWRtaW4=',
@@ -9,18 +9,38 @@ def get_printer_details():
         'Referer': f'http://{ip}/wireless_en.html',
     }
     res = requests.get("http://" + ip + '/wireless_en.html', headers=headers, timeout=tout)
-    # print(res.content)
     pattern = r'var\s+(\w+)\s*=\s*"([^"]+)"'
     matches = re.findall(pattern, str(res.content))
     result = dict(matches)
-    print(result)
-    payload = f'sta_setting_encry={result["sta_setting_encry"]}&sta_setting_auth={result["sta_setting_auth"]}&sta_setting_ssid={result["sta_setting_ssid"]}&sta_setting_auth_sel={result["sta_setting_auth_sel"]}&sta_setting_encry_sel={result["sta_setting_encry_sel"]}&sta_setting_type_sel={result["sta_setting_type_sel"]}&sta_setting_wpakey={result["sta_setting_wpakey"]}&wan_setting_dhcp={result["wan_setting_dhcp"]}&wan_setting_ip={result["wan_setting_ip"]}&wan_setting_msk={result["wan_setting_msk"]}&wan_setting_gw={result["wan_setting_gw"]}&wan_setting_dns={result["wan_setting_dns"]}'
+    payload = f'wan_setting_dhcp=STATIC&sta_setting_encry={result["sta_setting_encry"]}&sta_setting_auth={result["sta_setting_auth"]}&sta_setting_ssid={result["sta_setting_ssid"]}&sta_setting_auth_sel={result["sta_setting_auth"]}&sta_setting_encry_sel={result["sta_setting_encry"]}&sta_setting_type_sel=ASCII&sta_setting_wpakey={result["sta_setting_wpakey"]}&wan_setting_ip={result["wan_setting_ip"]}&wan_setting_msk={result["wan_setting_msk"]}&wan_setting_gw={result["wan_setting_gw"]}&wan_setting_dns={result["wan_setting_dns"]}'
     try:
         res = requests.post("http://" + ip + '/do_cmd_en.html', headers=headers, data=payload, timeout=tout)
         res2 = requests.post("http://" + ip + "/success_en.html", headers=headers, data='HF_PROCESS_CMD=RESTART',
                              timeout=tout)
+        print("Successfully set to static")
+    except Exception as e:
+        print(e)
+
+def set_printer_ip_dynamic():
+    ip = "192.168.1.159"
+    headers = {
+        'Authorization': 'Basic YWRtaW46YWRtaW4=',
+        'Origin': f'http://{ip}',
+        'Referer': f'http://{ip}/wireless_en.html',
+    }
+    res = requests.get("http://" + ip + '/wireless_en.html', headers=headers, timeout=tout)
+    pattern = r'var\s+(\w+)\s*=\s*"([^"]+)"'
+    matches = re.findall(pattern, str(res.content))
+    result = dict(matches)
+    payload = f'wan_setting_dhcp=DHCP&sta_setting_encry={result["sta_setting_encry"]}&sta_setting_auth={result["sta_setting_auth"]}&sta_setting_ssid={result["sta_setting_ssid"]}&sta_setting_auth_sel={result["sta_setting_auth"]}&sta_setting_encry_sel={result["sta_setting_encry"]}&sta_setting_type_sel=ASCII&sta_setting_wpakey={result["sta_setting_wpakey"]}'
+    try:
+        res = requests.post("http://" + ip + '/do_cmd_en.html', headers=headers, data=payload, timeout=tout)
+        res2 = requests.post("http://" + ip + "/success_en.html", headers=headers, data='HF_PROCESS_CMD=RESTART',
+                             timeout=tout)
+        print("Successfully set to static")
     except Exception as e:
         print(e)
 
 
-get_printer_details()
+
+set_printer_ip_dynamic()
