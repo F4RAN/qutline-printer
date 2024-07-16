766634b91b5bab40154172403c88e9e277b9737a
F4RAN <parsa.hassani30@gmail.com>
2024-07-16 23:52:35 +0330

    improve private ip finder

--- helpers/network.py
+++ helpers/network.py
@@ -1,29 +1,39 @@
 import socket
 import netifaces
+import re
 
 def get_private_ip():
+    def is_private_ip(ip):
+        private_ip_ranges = [
+            re.compile(r'^10\.'),
+            re.compile(r'^172\.(1[6-9]|2[0-9]|3[01])\.'),
+            re.compile(r'^192\.168\.')
+        ]
+        return any(pattern.match(ip) for pattern in private_ip_ranges)
+
     try:
         # Attempt to get IP address using socket (online)
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
             s.settimeout(2)
             s.connect(("8.8.8.8", 80))
             private_ip = s.getsockname()[0]
-            return private_ip
-
+            if is_private_ip(private_ip):
+                return private_ip
     except socket.error:
-        # Fallback: Try to get IP address using netifaces (offline)
-        try:
-            interfaces = netifaces.interfaces()
-            for iface in interfaces:
-                addrs = netifaces.ifaddresses(iface)
-                if netifaces.AF_INET in addrs:
-                    private_ip = addrs[netifaces.AF_INET][0]['addr']
-                    if private_ip != '127.0.0.1':
+        pass
+
+    # Fallback: Try to get IP address using netifaces (offline)
+    try:
+        interfaces = netifaces.interfaces()
+        for iface in interfaces:
+            addrs = netifaces.ifaddresses(iface)
+            if netifaces.AF_INET in addrs:
+                for addr_info in addrs[netifaces.AF_INET]:
+                    private_ip = addr_info['addr']
+                    if is_private_ip(private_ip):
                         return private_ip
-
-            print("Private IP not found.")
-            return None
-
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            return None
+        print("Private IP not found.")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
